03562c426e0baca2cd221fcd0d00f6fa447b1e6f
b1bametr <vovan200092@gmail.com>
2021-02-25 21:10:17 +0300

    ver 1

---
+++ Source/Dominator_of_steam/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿
+namespace Dominator_of_steam
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("SF Pro Display", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(12, 311);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(269, 46);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Очистить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.Gray;
+            this.textBox1.Font = new System.Drawing.Font("SF Pro Display", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(12, 49);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(269, 246);
+            this.textBox1.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.panel2.Controls.Add(this.panel1);
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Controls.Add(this.textBox1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(300, 370);
+            this.panel2.TabIndex = 3;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(83)))), ((int)(((byte)(83)))), ((int)(((byte)(83)))));
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(300, 27);
+            this.panel1.TabIndex = 2;
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Dominator_of_steam.Properties.Resources.hide;
+            this.pictureBox2.Location = new System.Drawing.Point(27, 6);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(16, 17);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox2.TabIndex = 7;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            this.pictureBox2.MouseEnter += new System.EventHandler(this.pictureBox2_MouseEnter);
+            this.pictureBox2.MouseLeave += new System.EventHandler(this.pictureBox2_MouseLeave);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Dominator_of_steam.Properties.Resources.close;
+            this.pictureBox1.Location = new System.Drawing.Point(6, 6);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(16, 16);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.MouseEnter += new System.EventHandler(this.pictureBox1_MouseEnter);
+            this.pictureBox1.MouseLeave += new System.EventHandler(this.pictureBox1_MouseLeave);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(300, 369);
+            this.Controls.Add(this.panel2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Source/Dominator_of_steam/Form1.cs
@@ -0,0 +1,199 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dominator_of_steam
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private static string Steam_Path;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Start_Program();
+            
+        }
+
+        private void Start_Program()
+        {
+
+            Steam_Get_Path();
+            Steam_Close();
+            Steam_Disk_Cleaner();
+            Steam_Reg_Cleaner();
+
+        }
+
+        private void Steam_Reg_Cleaner()//step 4
+        {
+            try
+            {
+                Registry.LocalMachine.DeleteSubKeyTree(@"SOFTWARE\WOW6432Node\Valve");
+                textBox1.Text += Environment.NewLine + "Запись №1 успешно удалена из реестра";
+            }
+            catch
+            {
+                textBox1.Text += Environment.NewLine + "Запись №1 в реестре не обнаружена";
+            }
+            try
+            {
+                Registry.CurrentUser.DeleteSubKeyTree(@"SOFTWARE\Valve");
+                textBox1.Text += Environment.NewLine + "Запись №2 успешно удалена из реестра";
+            }
+            catch
+            {
+                textBox1.Text += Environment.NewLine + "Запись №2 в реестре не обнаружена";
+            }
+
+        }
+
+        private void Steam_Disk_Cleaner()//step 3
+        {
+
+            if (File.Exists(Steam_Path + "\\config\\loginusers.vdf") == true)
+            {
+                File.Delete(Steam_Path + "\\config\\loginusers.vdf");
+                textBox1.Text += Environment.NewLine + "loginusers.vdf удален";
+            }
+            else
+            {
+                textBox1.Text += Environment.NewLine + "loginusers.vdf отсутствует";
+            }
+         
+            try
+            {
+                Directory.Delete(Steam_Path + "\\appcache\\stats", true);
+                textBox1.Text += Environment.NewLine + "stats удалена";
+            }
+            catch 
+            {
+                textBox1.Text += Environment.NewLine + "stats отсутствует";
+            }
+            try 
+            {
+                Directory.Delete(Steam_Path + "\\userdata", true);
+                textBox1.Text += Environment.NewLine + "userdata удалена";
+            }
+            catch (DirectoryNotFoundException)
+            {
+                textBox1.Text += Environment.NewLine + "userdata отсутствует";
+            }
+            bool Exist_File = false;
+            try
+            {
+                foreach (var f in Directory.GetFiles(Steam_Path))
+                {
+                    if (f.IndexOf("ssfn") != (-1))
+                    {
+                        File.Delete(f);
+                        textBox1.Text += Environment.NewLine + f + " удален";
+                        Exist_File = true;
+                    }
+                    if (Exist_File = false)
+                    {
+                        textBox1.Text += Environment.NewLine + "Файлы ssfn отсутствуют";
+                    }
+                }
+            }
+            catch
+            {
+               
+            }
+            
+
+
+
+        }
+        private void Steam_Get_Path() //steap 1
+        {
+            foreach (Process proc in Process.GetProcessesByName("steam"))
+            {
+                Steam_Path = proc.MainModule.FileName;
+            }
+
+            try
+            {
+                Steam_Path = Steam_Path.Substring(0, Steam_Path.Length - 10);
+                textBox1.Text = "Путь к папке найден: " + Steam_Path;
+            }
+            catch (System.NullReferenceException)
+            {
+                textBox1.Text = "Steam не обнаружен!";
+            }
+            
+           
+        }
+        private void Steam_Close() //steap 2
+        {
+            try
+            {
+                foreach (Process proc in Process.GetProcessesByName("steam"))
+                {
+                    proc.Kill();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        Point lastPoint;
+        private void pictureBox1_MouseEnter(object sender, EventArgs e)
+        {
+            this.Cursor = Cursors.Hand;
+        }
+
+        private void pictureBox1_MouseLeave(object sender, EventArgs e)
+        {
+            this.Cursor = Cursors.Arrow;
+        }
+
+        private void pictureBox2_MouseEnter(object sender, EventArgs e)
+        {
+            this.Cursor = Cursors.Hand;
+        }
+
+        private void pictureBox2_MouseLeave(object sender, EventArgs e)
+        {
+            this.Cursor = Cursors.Arrow;
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            lastPoint = new Point(e.X, e.Y);
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                this.Left += e.X - lastPoint.X;
+                this.Top += e.Y - lastPoint.Y;
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+    }
+}
